9244128ffd303309f523ca5227af556b36e2a6ea
zahriddinus <zahriddinulmasov99@gmail.com>
2026-06-13 15:01:18 +0500

    Bookmarket is working with full functionality

--- js/script.js
+++ js/script.js
@@ -6,8 +6,13 @@ const elFilmSelectCategories = document.querySelector(
 );
 const elFilmForm = document.querySelector(".film-form");
 const elFilmsList = document.querySelector(".films-list");
+const elItemBtnsWrapper = document.querySelector(".item-btns-wrapper");
+const elBookmarkedList = document.querySelector(".bookmarked-list");
+const elFilmInfoTitle = document.querySelector(".film-info-title");
+const elFilmInfoDesc = document.querySelector(".film-info-desc");
 
 const allFilm = films;
+const bookmarkedFilms = [];
 
 const generateGenres = function (filmsArr) {
   let uniqueGenres = [];
@@ -32,15 +37,11 @@ const renderCategories = function (categoryArr, htmlElement) {
     htmlElement.appendChild(newOption);
   });
 };
-
 renderCategories(generateGenres(allFilm), elFilmSelectCategories);
 
 elFilmForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
-  elFilmsList.innerHTML = null;
-
   const categoryValue = elFilmSelectCategories.value;
-
   let filteredFilms = [];
 
   for (let film of films) {
@@ -48,6 +49,7 @@ elFilmForm.addEventListener("submit", (evt) => {
       filteredFilms.push(film);
   }
 
+  elFilmsList.innerHTML = null;
   renderMovies(filteredFilms, elFilmsList);
 });
 
@@ -89,14 +91,28 @@ const renderMovies = function (filmsArr, htmlElement) {
     newFilmItemRating.style.fontSize = "19px";
     newFilmItemBtns.setAttribute(
       "class",
-      "d-flex gap-2 justify-content-between",
+      "item-btns-wrapper d-flex gap-2 justify-content-between",
     );
-    newFilmItemWatch.setAttribute("class", "btn btn-outline-primary");
+    newFilmItemWatch.setAttribute(
+      "class",
+      "item-watch-btn btn btn-outline-primary",
+    );
     newFilmItemWatch.textContent = "Watch trailer";
-    newFilmItemInfo.setAttribute("class", "btn btn-outline-secondary");
+    newFilmItemWatch.dataset.itemWatchId = film.id;
+    newFilmItemInfo.setAttribute(
+      "class",
+      "item-info-btn btn btn-outline-secondary",
+    );
     newFilmItemInfo.textContent = "More info";
-    newFilmItemBookmark.setAttribute("class", "btn btn-outline-success");
+    newFilmItemInfo.dataset.itemInfoBtnId = film.id;
+    newFilmItemInfo.setAttribute("data-bs-toggle", "modal");
+    newFilmItemInfo.setAttribute("data-bs-target", "#exampleModal");
+    newFilmItemBookmark.setAttribute(
+      "class",
+      "item-bookmark-btn btn btn-outline-success",
+    );
     newFilmItemBookmark.textContent = "Bookmark";
+    newFilmItemBookmark.dataset.itemBookmarkBtnId = film.id;
     newFilmItemGenres.textContent = film.genres.join(", ");
     newFilmItemGenres.setAttribute("class", "fs-5");
 
@@ -115,3 +131,109 @@ const renderMovies = function (filmsArr, htmlElement) {
   });
 };
 renderMovies(allFilm, elFilmsList);
+
+elFilmsList.addEventListener("click", (evt) => {
+  if (evt.target.matches(".item-watch-btn")) {
+    const filmWatchBtnId = evt.target.dataset.newFilmItemWatch;
+    const selectedWatch = allFilm.find((film) => film.id === filmWatchBtnId);
+
+    window.location.href =
+      "https://www.youtube.com/live/Ez0yd24SDiA?si=W3EyeWdA9hEznH8K";
+  }
+
+  if (evt.target.matches(".item-info-btn")) {
+    const filmInfoBtnId = evt.target.dataset.itemInfoBtnId;
+    const selectedInfo = allFilm.find((film) => film.id === filmInfoBtnId);
+
+    elFilmInfoTitle.textContent = selectedInfo.title;
+    elFilmInfoDesc.textContent = selectedInfo.overview;
+  }
+
+  if (evt.target.matches(".item-bookmark-btn")) {
+    const filmBookmarkBtnId = evt.target.dataset.itemBookmarkBtnId;
+    const selectedFilm = allFilm.find((film) => film.id === filmBookmarkBtnId);
+
+    if (!bookmarkedFilms.includes(selectedFilm))
+      bookmarkedFilms.push(selectedFilm);
+
+    elBookmarkedList.innerHTML = null;
+  }
+
+  renderBookmarked(bookmarkedFilms, elBookmarkedList);
+});
+
+const renderBookmarked = function (bookmarkedArr, htmlElement) {
+  if (bookmarkedArr.length > 0) {
+    bookmarkedArr.forEach((bookmarked) => {
+      const newItem = document.createElement("li");
+      const newItemTitle = document.createElement("h4");
+      const newItemBtnsWrapper = document.createElement("div");
+      const newItemInfoDiv = document.createElement("div");
+      const newItemInfoBtn = document.createElement("button");
+      const newItemDeleteDiv = document.createElement("div");
+      const newItemDeleteBtn = document.createElement("button");
+
+      newItem.setAttribute("class", "bookmarked-items p-3");
+      newItemTitle.textContent = bookmarked.title;
+      newItemTitle.setAttribute("class", "m-0 mb-2 fs-5");
+      newItemBtnsWrapper.setAttribute("class", "btns-wrapper row gx-2");
+      newItemInfoDiv.setAttribute("class", "col");
+      newItemInfoBtn.setAttribute(
+        "class",
+        "bookmarked-info-btn w-100 btn btn-outline-secondary",
+      );
+      newItemInfoBtn.dataset.bookmarkedDeletBtnId = bookmarked.id;
+      newItemInfoBtn.textContent = "More info";
+      newItemInfoBtn.setAttribute("data-bs-toggle", "modal");
+      newItemInfoBtn.setAttribute("data-bs-target", "#exampleModal");
+      newItemDeleteDiv.setAttribute("class", "col");
+      newItemDeleteBtn.setAttribute(
+        "class",
+        "item-delete-btn w-100 btn btn-danger",
+      );
+      newItemDeleteBtn.textContent = "Delete";
+      newItemDeleteBtn.dataset.deleteBtnId = bookmarked.id;
+
+      htmlElement.appendChild(newItem);
+      newItem.appendChild(newItemTitle);
+      newItem.appendChild(newItemBtnsWrapper);
+      newItemBtnsWrapper.appendChild(newItemInfoDiv);
+      newItemInfoDiv.appendChild(newItemInfoBtn);
+      newItemBtnsWrapper.appendChild(newItemDeleteDiv);
+      newItemDeleteDiv.appendChild(newItemDeleteBtn);
+    });
+  } else {
+    const newItem = document.createElement("li");
+    const newItemTitle = document.createElement("h4");
+
+    newItem.setAttribute("class", "bookmarked-items p-3");
+    newItemTitle.setAttribute("class", "m-0 fs-5 text-danger");
+    newItemTitle.textContent = "Nothing here yet!";
+
+    htmlElement.appendChild(newItem);
+    newItem.appendChild(newItemTitle);
+  }
+};
+
+elBookmarkedList.addEventListener("click", (evt) => {
+  if (evt.target.matches(".item-delete-btn")) {
+    const bookmarkedDelete = evt.target.dataset.deleteBtnId;
+    const foundedFilmDelete = bookmarkedFilms.find(
+      (film) => film.id === bookmarkedDelete,
+    );
+    bookmarkedFilms.splice(foundedFilmDelete, 1);
+
+    elBookmarkedList.innerHTML = null;
+    renderBookmarked(bookmarkedFilms, elBookmarkedList);
+  }
+
+  if (evt.target.matches(".bookmarked-info-btn")) {
+    const bookmarkedInfo = evt.target.dataset.bookmarkedDeletBtnId;
+    const foundedFilmInfo = bookmarkedFilms.find(
+      (film) => film.id === bookmarkedInfo,
+    );
+
+    elFilmInfoTitle.textContent = foundedFilmInfo.title;
+    elFilmInfoDesc.textContent = foundedFilmInfo.overview;
+  }
+});
